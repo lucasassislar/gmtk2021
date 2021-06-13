8c5780e4ed6b8e5a17a20e3a2f9a215496d30fbe
Lucas Assis <lucasassis@outlook.com>
2021-06-13 09:22:08 -0300

    client working

---
+++ gmtk2021/Assets/codebase/ChainedWithMe/components/NetworkGameManager.cs
@@ -0,0 +1,24 @@
+﻿using MLAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ChainedWithMe {
+    public class NetworkGameManager : MonoBehaviour {
+        private void Start() {
+            NetworkManager.Singleton.OnClientConnectedCallback += Singleton_OnClientConnectedCallback;
+            NetworkManager.Singleton.OnClientDisconnectCallback += Singleton_OnClientDisconnectCallback;
+        }
+
+        private void Singleton_OnClientDisconnectCallback(ulong obj) {
+            SceneManager.LoadScene("MainMenu");
+        }
+
+        private void Singleton_OnClientConnectedCallback(ulong obj) {
+        }
+    }
+}
