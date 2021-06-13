e10ac6461af22850e1ee68cee7afa12beac9de33
Lucas Assis <lucasassis@outlook.com>
2021-06-13 08:56:14 -0300

    intro scene working

---
+++ gmtk2021/Assets/codebase/ChainedWithMe/components/system/MenuLoadComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ChainedWithMe.System {
+    public class MenuLoadComponent : MonoBehaviour {
+
+        public void LoadMenu() {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
