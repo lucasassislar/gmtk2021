42c0e5b698b40fd809e496603dbef03dbfa8769e
Lucas Assis <lucasassis@outlook.com>
2021-06-14 20:43:56 -0300

    added opennat

--- gmtk2021/Assets/codebase/ChainedWithMe/components/system/MainMenuManager.cs
+++ gmtk2021/Assets/codebase/ChainedWithMe/components/system/MainMenuManager.cs
@@ -1,9 +1,11 @@
 ﻿using MLAPI;
 using MLAPI.Transports.UNET;
+using Open.Nat;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -11,7 +13,33 @@ using UnityEngine.UI;
 
 namespace ChainedWithMe.System {
     public class MainMenuManager : MonoBehaviour {
+        public Button showPublicIP;
+        public Text publicIP;
+
         public InputField fieldIP;
+
+        public string strIP = "0.0.0.0";
+
+        private void Start() {
+            var discoverer = new NatDiscoverer();
+            discoverer.DiscoverDeviceAsync().ContinueWith(device => {
+                var discoverer = new NatDiscoverer();
+                var cts = new CancellationTokenSource(10000);
+                device.Result.CreatePortMapAsync(new Mapping(Protocol.Udp, 52000, 52000, "Guide Me Please (UDP)"));
+
+                device.Result.GetExternalIPAsync().ContinueWith(ip => {                    
+                    strIP = ip.Result.ToString();
+                });
+            });
+        }
+
+        private void Update() {
+            publicIP.text = strIP;
+        }
+
+        public void ShowPublicIP() {
+            showPublicIP.gameObject.SetActive(false);
+        }
 
         public void Host() {
             Globals.IsHost = true;
@@ -26,6 +54,7 @@ namespace ChainedWithMe.System {
 
             string ipAddress = fieldIP.text;
             unet.ConnectAddress = ipAddress;
+
 
             SceneManager.LoadScene("ChainedWithMe");
         }
